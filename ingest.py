2dd0eba69ba1f1303313f21439f5168c49ccd389
Dylan McReynolds <dmcreynolds@lbl.gov>
2021-02-12 12:33:45 -0800

    initial

---
+++ ingest.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+import requests
+
+session = requests.Session()
+response = requests.post(url="http://localhost:3000/api/v3/Users/login", data={"username": "ingestor", "password": "aman"})
+pprint(response.json())
+token = response.json()['id']
+
+payload = {
+ "creationLocation": "/PSI/SLS/TOMCAT3",
+ "creationTime": "	2021-02-10T23:56:37Z",
+ "sourceFolder": "/scratch/devops",
+ "type": "raw",
+ "owner": "ingestor",
+ "ownerGroup":"p16623",
+ "contactEmail": "dmcreynolds@lbl.gov",
+ "shareGroup": "foo"
+}
+
+response = session.post(url=f"http://localhost:3000/api/v3/Datasets?access_token={token}", data=payload)
+pprint(response.json())
